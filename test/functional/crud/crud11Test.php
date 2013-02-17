d0d194bc35c4666c32e7647f86bf8e31de95798c
e1himself <planet.axis@gmail.com>
2013-02-17 22:48:52 +0200

    Implement functional testing for issue fixed by pull request #183

--- test/functional/crud/crud11Test.php
+++ test/functional/crud/crud11Test.php
@@ -16,9 +16,10 @@ if (!include(dirname(__FILE__).'/../../bootstrap/functional.php'))
 }
 
 require_once(dirname(__FILE__).'/crudBrowser.class.php');
+require_once(dirname(__FILE__).'/crudBrowserAdmin15.class.php');
 
-$b = new CrudBrowser();
-$b->browse(array('with-show', 'route-prefix=acme_article'), array(
+$b = new CrudBrowserAdmin15();
+$b->browse(array('with-show', 'route-prefix=acme_article', 'theme=admin15', 'generate-in-cache'), array(
   'urlPrefix' => 'articles',
   'moduleName' => 'article_backend',
 ));
